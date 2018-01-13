f07fb5d83e00ead6fdf73c63ac757ca5978cb54c
Patrick Knight <ptknight00@gmail.com>
2018-01-13 13:46:02 -0600

    cull ajax beer pull

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -28,13 +28,33 @@ function pullCrypto(){
 }
 
 // pulling beer data
+// beer variables 
+var abv = Math.abs(5.8);
+var abvLower = Math.floor(abv);
+var abvHigher = Math.ceil(abv);
+var filterResult = [];
+var beerArray = [];
+var roundingArray = [];
+
 function pullBeer(){
-    var queryURL = "https://api.punkapi.com/v2/beers";
+    var queryURL = "https://api.punkapi.com/v2/beers?abv_gt=" + abvLower + "&abv_lt=" + abvHigher;
     $.ajax({
     url: queryURL,
     method: "GET"
     }).done(function(beerResponse) {
         console.log(beerResponse);
+        for (i = 0; i < beerResponse.length; i++){
+            var returnABV = beerResponse[i].abv
+            filterResult.push(returnABV);
+            if (returnABV === abv) {
+                beerArray.push(i);
+            } 
+        }
+        console.log(filterResult);
+        console.log(beerArray);
+        for (i = 0; i < beerArray.length; i++) {
+            console.log(beerResponse[i]);
+        }
     });
 }
 
