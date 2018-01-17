3aaad5a463841daea0d744915bea1f47ef683d7f
Max <lfmax223@gmail.com>
2018-01-16 20:09:20 -0600

    Update search currency dropdown configuration

--- assets/javascript/typeahead.js
+++ assets/javascript/typeahead.js
@@ -1,35 +1,13 @@
-var substringMatcher = function (strs) {
-    return function findMatches(q, cb) {
-        var matches, substringRegex;
-
-        // an array that will be populated with substring matches
-        matches = [];
-
-        // regex used to determine if a string contains the substring `q`
-        substrRegex = new RegExp(q, 'i');
-
-        // iterate through the pool of strings and for any string that
-        // contains the substring `q`, add it to the `matches` array
-        $.each(strs, function (i, str) {
-            if (substrRegex.test(str)) {
-                matches.push(str);
-            }
-        });
-
-        cb(matches);
-    };
-};
-
 
 // Populate currency list from Currency API
 function populateCurrencyNames() {
 
-    pullCrypto(function(cryptoList) {
-        var currencyNames = [];
+    pullCrypto(function (cryptoList) {
+        let currencyNames = [];
 
         // Store "Name (Symbol)" for each currency"
-        for (var i = 0; i < cryptoList.length; i++) {
-            var currencyName = `${cryptoList[i].name} (${cryptoList[i].symbol})`
+        for (let i = 0; i < cryptoList.length; i++) {
+            let currencyName = `${cryptoList[i].name} (${cryptoList[i].symbol})`;
             currencyNames.push(currencyName);
         }
         // Store in firebase
@@ -38,28 +16,33 @@ function populateCurrencyNames() {
 }
 
 
-
 // Populate the search bar with currency names from firebase
 database.ref("/currencyNames").once("value", function (snapshot) {
-    var currencyNames = snapshot.val();
-    if(!currencyNames){
+    let currencyNames = snapshot.val();
+    if (!currencyNames) {
+        // Refresh data from API
         populateCurrencyNames();
     } else {
-        // $('.typeahead').typeahead({
-        //     hint: true,
-        //     highlight: true,
-        //     minLength: 1
-        // },
-        //     {
-        //         name: 'currencies',
-        //         source: substringMatcher(currencyNames)
-        //     });
-        $('.typeahead').easyAutocomplete({ data: currencyNames, theme: "round"});
+        configureSearchBar(currencyNames);
     }
-
-  
 }, function (errorObject) {
     console.log("The read failed: " + errorObject.code);
 });
 
+////////////  Configuration and functions for the easyautocomplete plugin ////////////
+function configureSearchBar(currencyNames) {
+    // Config for easyautocomplete plugin
+    let config = {
+        data: currencyNames, list: {
+            match: {
+                enabled: true
+            }
+        }, theme: "square", cssClasses: "form-control form-control-sm fix-autocomplete input-group",
 
+    };
+    $('.typeahead').easyAutocomplete(config);
+    // Fix for working with bootstrap input-group and alignment
+    let $easyContainer = $(".easy-autocomplete-container");
+    $easyContainer.css("text-align", "left");
+    $easyContainer.css("margin-top", "40px");
+}
