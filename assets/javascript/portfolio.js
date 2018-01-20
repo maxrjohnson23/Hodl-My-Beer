6d979af10c1b00d3c43a8ec6911a7ffe4cbedc1b
Max <lfmax223@gmail.com>
2018-01-19 20:31:41 -0600

    Add cryptocompare graph to portfolio page

--- assets/javascript/portfolio.js
+++ assets/javascript/portfolio.js
@@ -1,24 +1,31 @@
 database.ref("/cryptoList").on("value", function (snapshot) {
-    var currencyList = snapshot.val();
+    let currencyList = snapshot.val();
 
     if (currencyList) {
-        for (var i = 0; i < 25; i++) {
-            var currency = currencyList[i];
+        for (let i = 0; i < 25; i++) {
+            let currency = currencyList[i];
             // Create row for data table
             dataTable.row.add([
+                `<img src="assets/images/graph.png" class="graph-icon" data-currency="${currency.symbol}" />`,
                 currency.symbol,
                 currency.name,
                 currency.percent_change_1h,
                 currency.percent_change_24h,
                 currency.percent_change_7d,
-                `<button id="remove_${currency.symbol}"class='btn-sm btn-danger'>Remove</button>`
+                "<button class='btn-sm btn-danger'>Remove</button>"
             ]).draw(false);
         }
     }
 });
 
 // Initialize data tables on portfolio
-var dataTable = $('#portfolio-table').DataTable();
+const dataTable = $('#portfolio-table').DataTable({
+    "order": [[1, 'asc']],
+    "columnDefs": [{
+        "targets": 'no-sort',
+        "orderable": false,
+    }]
+});
 
 // Remove functionality for rows in the table
 $('#portfolio-table tbody').on('click', 'button.btn-danger', function () {
@@ -27,5 +34,24 @@ $('#portfolio-table tbody').on('click', 'button.btn-danger', function () {
     // TODO delete from firebase
 });
 
+// Click event handler for graph icon to show the cryptocompare chart
+$(document).on('click', '.graph-icon', function () {
+    let symbol = $(this).data("currency");
+    // Remove any previous chart
+    $("#currency-graph").empty();
 
+    // CryptoCompare graph widget
+    let appName = encodeURIComponent(window.location.hostname);
+    if (appName === "") {
+        appName = "local";
+    }
+    // anchored to script tag for dynamic loading
+    let s = document.createElement("script");
+    s.type = "text/javascript";
+    s.async = true;
+    let theUrl = `https://widgets.cryptocompare.com/serve/v3/coin/chart?fsym=${symbol}&tsyms=USD`;
+    s.src = theUrl + (theUrl.indexOf("?") >= 0 ? "&" : "?") + "app=" + appName;
+    document.getElementById("currency-graph").appendChild(s);
+
+});
 
