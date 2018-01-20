d5c1ede917681fb824243be3e50ae0b079629b32
Zach Johnson <zachtjohnson01@gmail.com>
2018-01-20 13:02:32 -0600

    Bug fix

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -57,12 +57,11 @@ loginRef.on('value', function(snapshot) {
 // });
 
 //Get the firebase reference    
-var ref = new Firebase("https://hodl-my-beer.firebaseio.com");
-    ref.onAuth(function(authData) {
+    firebase.auth().onAuthStateChanged(function(authData) {
       if (authData && isNewUser) {
         // save the user's profile into Firebase so we can list users,
         // use them in Security and Firebase Rules, and show profiles
-        database.child("users").child(authData.uid).set({
+        userRef.child(authData.uid).set({
             provider: authData.provider,
             name: getName(authData)
           //some more user data
@@ -72,7 +71,7 @@ var ref = new Firebase("https://hodl-my-beer.firebaseio.com");
 
 //Get the correct firebase reference
 // var ref = new Firebase("https://<YOUR-FIREBASE-APP>.firebaseio.com").child("users").child(authData.uid);
-var UserChildRef = firebase.child("users").child(authData.uid);
+var UserChildRef = userRef.child(authData.uid);
 //Get the data
 UserChildRef.once("value", function(data) {
   // do some stuff once, user data will be in the variable data
